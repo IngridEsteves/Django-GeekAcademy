b7e33de8f22574a6e2de8f26cc131f9ccef3a8b2
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-04-21 18:00:29 -0300

    template

--- django1/django1/core/urls.py
+++ django1/django1/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('contato', contato),
+    path('', index, name='index'),
+    path('contato', contato, name='contato'),
+    path('produto/<int:pk>', produto, name='produto'),
 ]
