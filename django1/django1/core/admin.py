dbe52a1b34336316322c019993e7dddd89f1fe2f
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-04-21 13:10:47 -0300

    bd

--- django1/django1/core/admin.py
+++ django1/django1/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'valor', 'estoque')
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
